1cd76240c0f9ebc73db131da46146d412d563b91
Jason Yost <jyost@gocortexlabs.com>
2015-07-18 17:37:27 -0600

    Sweet scrolling action

--- src/jquery.numble.js
+++ src/jquery.numble.js
@@ -1,61 +1,99 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+;
+(function($, window, document, undefined) {
 
 	"use strict";
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
+	// undefined is used here as the undefined global variable in ECMAScript 3 is
+	// mutable (ie. it can be changed by someone else). undefined isn't really being
+	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+	// can no longer be modified.
 
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
+	// window and document are passed through as local variable rather than global
+	// as this (slightly) quickens the resolution process and can be more efficiently
+	// minified (especially when both are regularly referenced in your plugin).
 
-		// Create the defaults once
-		var pluginName = "numble",
-				defaults = {
-				propertyName: "value"
+	// Create the defaults once
+	var pluginName = "numble",
+		defaults = {
+			debug: false
 		};
 
-		// The actual plugin constructor
-		function Plugin ( element, options ) {
-				this.element = element;
-				// jQuery has an extend method which merges the contents of two or
-				// more objects, storing the result in the first object. The first object
-				// is generally empty as we don't want to alter the default options for
-				// future instances of the plugin
-				this.settings = $.extend( {}, defaults, options );
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
-		}
+	// The actual plugin constructor
+	function Plugin(element, options) {
+		this.element = element;
+		// jQuery has an extend method which merges the contents of two or
+		// more objects, storing the result in the first object. The first object
+		// is generally empty as we don't want to alter the default options for
+		// future instances of the plugin
+		this.settings = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
 
-		// Avoid Plugin.prototype conflicts
-		$.extend(Plugin.prototype, {
-				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.settings).
-						console.log("xD");
-				},
-				yourOtherFunction: function () {
-						// some logic
+	// Avoid Plugin.prototype conflicts
+	$.extend(Plugin.prototype, {
+		init: function() {
+			// Place initialization logic here
+			// You already have access to the DOM element and
+			// the options via the instance, e.g. this.element
+			// and this.settings
+			// you can add more functions like the one below and
+			// call them like so: this.yourOtherFunction(this.element, this.settings).
+			this.debugMessage("numble initialized", this.settings);
+			this.setupControls(this.element, this.settings);
+		},
+		setupControls: function(element, settings) {
+
+			// Hide the original control to prevent default browser styling interference
+			$(element).addClass('numble-original');
+			$(element).hide();
+
+			// Inject a new element into the page to handle the display control of the numbers
+			$(element).after('<div class="numble-control"></div>');
+			var control = $(element).siblings('.numble-control');
+
+			// Display the original value of the control
+			var originalValue = parseInt($(element).val()) || 0;
+			this.debugMessage("original value " + originalValue, this.settings);
+			control.text(originalValue);
+
+			// bind the mouse wheel to the control
+			control.bind('mousewheel DOMMouseScroll', function(event) {
+				var val = parseInt($(element).val()) || 0;
+				if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+					console.log('received scroll up event');
+					val++;
+					$(this).text(val);
+					$(element).val(val);
+					console.log('incrementing to ' + val);
+				} else {
+					console.log('received scroll down event');
+					val--;
+					$(this).text(val);
+					$(element).val(val)
+					console.log('decrementing to ' + val);
 				}
+			});
+
+		},
+		debugMessage: function(message, settings) {
+			if (settings.debug) {
+				console.log(message);
+			}
+		}
+	});
+
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			if (!$.data(this, "plugin_" + pluginName)) {
+				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
+			}
 		});
+	};
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
-				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
-		};
-
-})( jQuery, window, document );
+})(jQuery, window, document);
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,78 @@
-(function () {
-  'use strict';
+(function() {
+	'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(true).toBe(true);
+	describe('.numble()', function() {
+
+		describe('init', function() {
+			it('should add .numble-original class to the element', function() {
+				expect($('.numble-original')).toBeInDOM();
+			});
+
+			it('should hide the display of the original control', function() {
+				expect($('.numble-original')).toBeHidden();
+			});
+
+			it('should inject a display control into the page', function() {
+				expect($('.numble-control')).toBeInDOM();
+			});
+
+			describe('given an existing numerical value', function() {
+				it('should display the value in the new element', function() {
+					// expect($('.value')).toContainText('42');
+          pending();
+				});
+			});
+
+      describe('given no original value', function(){
+        it('should set the initial number to zero', function(){
+          expect($('.no-value')).toContainText('0');
+        });
       });
-    });
-  });
+
+      // FIXME Need to test the scroll event on the mouse, the code is working but I am unable to
+      // determine how to send the mouse wheel event to the page
+			describe('mouse wheel control', function() {
+
+				describe('in Firefox', function() {
+					it('should bind the element to the DOMMouseScroll event', function() {
+
+            // var scrollEvent = spyOnEvent('.numble-control', 'DOMMouseScroll');
+						// window.ChromeWheel = function() {
+						// 	var evt = document.createEvent("MouseEvents");
+						// 	evt.initMouseEvent(
+						// 		'DOMMouseScroll', // in DOMString typeArg,
+						// 		true, // in boolean canBubbleArg,
+						// 		true, // in boolean cancelableArg,
+						// 		window, // in views::AbstractView viewArg,
+						// 		120, // in long detailArg,
+						// 		0, // in long screenXArg,
+						// 		0, // in long screenYArg,
+						// 		0, // in long clientXArg,
+						// 		0, // in long clientYArg,
+						// 		0, // in boolean ctrlKeyArg,
+						// 		0, // in boolean altKeyArg,
+						// 		0, // in boolean shiftKeyArg,
+						// 		0, // in boolean metaKeyArg,
+						// 		0, // in unsigned short buttonArg,
+						// 		null // in EventTarget relatedTargetArg
+						// 	);
+						// 	$('.numble-control').dispatchEvent(evt);
+						// }
+            // $('.numble-control').trigger("DOMMouseScroll");
+            // expect("DOMMouseScroll").toHaveBeenTriggeredOn('.numble-control');
+            // expect(scrollEvent).toHaveBeenTriggered();
+            pending();
+					});
+				});
+
+        describe('in Webkit', function(){
+          it('should bind to the mousewheel event', function(){
+            pending();
+          });
+        });
+
+			});
+		});
+
+	});
 })();
